243628df06f311282a46692a16f0f79f0e57db23
Ivan Biagi <H0nus@users.noreply.github.com>
2017-06-12 14:46:59 +0200

    Create msf.py

--- core/msf.py
+++ core/msf.py
@@ -15,7 +15,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# Created by _R4v3n Tech
+# Created by H0nus
 
 import sys
 from core import pycolor
