a6e6dbc9ede92f15239356f2a0fc57447b6c2a4c
Ivan Biagi <H0nus@users.noreply.github.com>
2017-06-12 14:46:12 +0200

    Update spynoteshell.py

--- spynoteshell.py
+++ spynoteshell.py
@@ -16,7 +16,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# Created by B4ckp0r7
+# Created by H0nus
 
 import os
 import re
